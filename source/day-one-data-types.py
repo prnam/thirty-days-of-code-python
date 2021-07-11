05192543f2c0156e9e4676267a7836cbc3382095
Pranam <68323012+prnam@users.noreply.github.com>
2021-07-11 12:11:53 +0530

    cosmetic changes

--- source/day-one-data-types.py
+++ source/day-one-data-types.py
@@ -2,13 +2,13 @@ i = 4
 d = 4.0
 s = 'HackerRank '
 # Declare second integer, double, and String variables.
-secondInteger=0
-secondDouble=0.0
-secondString=""
+secondInteger = 0
+secondDouble = 0.0
+secondString = ""
 # Read and save an integer, double, and String to your variables.
-secondInteger=int(input())
-secondDouble=float(input())
-secondString=str(input())
+secondInteger = int(input())
+secondDouble = float(input())
+secondString = str(input())
 # Print the sum of both integer variables on a new line.
 print(i+secondInteger)
 # Print the sum of the double variables on a new line.
